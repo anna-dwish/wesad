c6a9e6693a55e7fdc313e9f4e346e293f6b4d29d
Anna Darwish <ard51@duke.edu>
2020-09-20 22:35:59 -0400

    Added emp4 data to csv with naive repeats to align w chest data

--- load_wesad.py
+++ load_wesad.py
@@ -11,6 +11,7 @@ CHEST = "chest"
 WRIST = "wrist"
 LABEL = "label"
 METRICS = ["EMG", "ECG", "EDA", "Temp", "Resp"]
+WATCH_METRICS = ["EDA", "TEMP", "HR"]
 PATH = "/hpc/group/sta440-f20/WESAD/WESAD/"
 
 STRESS = 2
@@ -32,12 +33,29 @@ merged with the rest of the sensor
 """
 
 
-def load_pkl(file_name, s_id, type=CHEST):
+def generate_subject_df(file_path, s_id):
+    file_name = file_path + s_id + ".pkl"
     data = pd.read_pickle(file_name)
     df = pd.DataFrame()
+    df_wrist = pd.DataFrame()
 
     for m in METRICS:
         df[m] = list(flatten(data[SIGNAL][CHEST][m]))
+
+    for m in WRIST_METRICS:
+        df_wrist = pd.read_csv(file_path + s_id + EMP_DATA + "/" + m + ".csv", header=None)
+
+        up_sampling_factor = int(len(df) / len(df_wrist))
+        df_wrist = df_wrist.loc[df_wrist.index.repeat(up_sampling_factor)]
+
+        if len(df_wrist) != len(df):
+            # extend the last row for the remaining ones if uneven division
+            df_wrist_last = df_wrist.iloc[[-1]]
+            df_wrist_last = df_wrist_last.loc[df_wrist_last.index.repeat(len(df) - len(df_wrist))]
+
+            df_wrist = pd.concat([df_wrist, df_wrist_last], ignore_index=True)
+
+        df[m + "_EMP4"] = list(df_wrist.iloc[:, 0])
 
     df[LABEL] = data[LABEL]
     df = df.loc[df[LABEL].isin([STRESS, AMUSEMENT])]
@@ -48,14 +66,13 @@ def load_pkl(file_name, s_id, type=CHEST):
     return df
 
 
-chest_df = load_pkl(PATH + "S2/S2.pkl", "S2")
+merged_df = generate_subject_df(PATH + "S2/", "S2")
 
 for s in subjects[1:]:
     sid = "S" + str(s)
-    curr_df = load_pkl(PATH + sid + "/" + sid + ".pkl", "S" + str(s))
-    chest_df = chest_df.append(curr_df)
+    current_subject_df = generate_subject_df(PATH + sid + "/", "S" + str(s))
+    merged_df = merged_df.append(current_subject_df)
 
 
-chest_df.to_csv("merged_chest.csv")
-
+merged_df.to_csv("merged_wesad.csv")
 
